f7cbb1ad38d808cd903576c6447149ad041cadd9
Daitq <tranquangdai1219@gmail.com>
2024-10-24 17:58:59 +0700

    upd code

---
+++ components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import React from "react";
+
+export default (props: {routerLink?: string}) => {
+    return <>
+        <Link href={props.routerLink || '/'}>
+            <img src="./images/header/logo-tapchikhoahoc.svg" alt={"Logo tạp chí khoa học"} />
+        </Link>
+    </>
+}
